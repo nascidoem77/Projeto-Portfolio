5cb9f6188188d7c45bab9895903d0c846ead4ff7
nascidoem77 <nascidoem77@gmail.com>
2021-10-20 21:46:40 -0300

    habilidades done

--- static/script.js
+++ static/script.js
@@ -9,3 +9,24 @@ menuMobile.addEventListener('click', () => {
     : menuMobile.classList.replace("bi-x", "bi-list")
     body.classList.toggle("menu-nav-active")
 })
+
+// Fecha o menu (no modo mobile) quando clicar em algum item
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener('click', () => {
+        if (body.classList.contains('menu-nav-active')) {
+            body.classList.remove('menu-nav-active')
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
+})
+
+// Scroll da página home ao passar o mouse sobre a seta
+
+document.querySelector('.scroll').addEventListener('mouseenter', () => {
+    document.getElementById("sobre").scrollIntoView({behavior:'smooth'});
+    });
+
+    
